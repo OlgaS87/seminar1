216ab9cd15fcc674ffa9b55a29625574f83ee28e
Oiga Serik <Kharitonova.87@mail.ru>
2022-12-17 18:37:34 +0300

    lecture

---
+++ lec.py
@@ -0,0 +1,18 @@
+#value = None
+#a = 123
+#b = 1.23
+#print (a)
+#print (b)
+#value = 12334
+#print (value)
+#s = 'hello world'
+#print(s) # вывод строки
+#print(a,' - ', b, s)
+#print(f'{a} - {b} - {s}')
+#f = True
+#print(f)
+
+#List = ['1', '2', '3']
+#print(List)
+a = 1 > 4
+print(a)
